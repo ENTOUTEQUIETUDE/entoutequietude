9454c2b53aa4b687daf20504606981b559cb0f3f
Angie727 <kimtiers@gmail.com>
2025-06-04 16:17:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function sendMail(){
     let parms = {
-        name : document.getElementById('name').value,
-        email : document.getElementById("mail").value,
+        nom : document.getElementById('naom').value,
+        email : document.getElementById("email").value,
+        sujet : document.getElementById('sujet').value,
         message : document.getElementById("message").value,
-        numero : document.getElementById("num").value,
+        numero : document.getElementById("numero").value,
     } 
     emailjs.send("service_uro0v3i","template_kou6wdl",parms).then(alert("Message envoyé !"))
 }
