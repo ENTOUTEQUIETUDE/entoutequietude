ede41f58121b8dcf00b9aebc863533bb73e616cb
ENTOUTEQUIETUDE <betty.reiki@outlook.fr>
2025-06-27 19:15:35 +0200

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -4,8 +4,6 @@ showSlides(slideIndex);
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
-
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -19,5 +17,9 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 } 
